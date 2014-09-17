2516f05e2c95eb1f5bb91702dc2185c38dfdadc3
Howard Tyson <howard@howardtyson.com>
2014-09-17 09:00:58 -0400

    Adding test for generic load method.

---
+++ test/loadTest.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var path = require('path');
+
+var Loader = require('..');
+
+function fixture(name) {
+  return path.resolve(path.join(__dirname, 'fixtures', 'fileLoader', name) + '.yaml');
+};
+
+describe('yaml-config-loader', function() {
+  it('should accept an object', function() {
+  });
+});
+ 
