05571b18b554845093aa2523d8630d03213a7d9c
Howard Tyson <howard@howardtyson.com>
2015-10-25 21:26:11 -0400

    Adding testing for error handling options.

---
+++ test/errorHandling.js
@@ -0,0 +1,21 @@
+var should = require('should');
+
+var Loader = require('..');
+
+describe('yaml-config-loader', function() {
+  describe('errorHandling', function() {
+    it('should emit an error if a non-existant file is added', function(done) {
+      var loader = new Loader({ stopOnError: false });
+      loader.addFile('no-file');
+      loader.on('error', function(error) {
+        should.exist(error);
+        error.message.should.equal('Specified configuration file `no-file` not found.');
+        done();
+      });
+      loader.load(function(error, config) {
+        should.not.exist(error);
+      });
+    });
+  });
+});
+ 
